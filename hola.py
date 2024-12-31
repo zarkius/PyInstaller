0575d08d5e221f0b6de14a8512e0b3ca719fb9d4
zarkius <diegomallor2.0@gmail.com>
2024-12-31 12:43:34 +0100

    ejemplo calculadora

--- hola.py
+++ hola.py
@@ -1,5 +1,28 @@
-def suma(a, b):
-    return a + b
+class Calculadora:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
-resultado = suma(2, 2)
-print(f"La suma de 2 y 2 es: {resultado}")
+    def suma(self):
+        return self.a + self.b
+
+    def resta(self):
+        return self.a - self.b
+
+    def multiplicacion(self):
+        return self.a * self.b
+
+    def division(self):
+        if self.b != 0:
+            return self.a / self.b
+        else:
+            return "Error: División por cero"
+
+a = float(input("Ingrese el primer número: "))
+b = float(input("Ingrese el segundo número: "))
+calculadora = Calculadora(a, b)
+
+print("Suma:", calculadora.suma())
+print("Resta:", calculadora.resta())
+print("Multiplicación:", calculadora.multiplicacion())
+print("División:", calculadora.division())
